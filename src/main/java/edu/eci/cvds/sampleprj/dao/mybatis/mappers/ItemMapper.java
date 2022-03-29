7ada4684b0ac36adf2d8de0692153e83ba203b1d
david <david.leon-m@mail.escuelaing.edu.co>
2022-03-28 22:13:21 -0500

    Taller anterior

---
+++ src/main/java/edu/eci/cvds/sampleprj/dao/mybatis/mappers/ItemMapper.java
@@ -0,0 +1,20 @@
+package edu.eci.cvds.sampleprj.dao.mybatis.mappers;
+
+
+import java.util.Date;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+import edu.eci.cvds.samples.entities.Item;
+
+public interface ItemMapper {
+    
+    
+    public List<Item> consultarItems();        
+    
+    public Item consultarItem(@Param("idt") int id);
+    
+    public void insertarItem(@Param("item")Item it);
+
+        
+}
