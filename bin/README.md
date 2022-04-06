538f508f989375f4a7020adea29624728fd9e6c0
david <david.leon-m@mail.escuelaing.edu.co>
2022-04-05 20:07:28 -0500

    Pruebas

--- bin/README.md
+++ bin/README.md
@@ -1,2 +1,36 @@
 ## lab08_cvds_2022
 ## PARTE I. INICIO EN CLASE
+Cree la interface ItemDAO y utilicela como referencia para realizar los Objetos de Acceso a los Datos (DAO) para las demas entidades:\
+![image](https://user-images.githubusercontent.com/98216838/160669244-d0732236-9a23-4a84-8d0b-11e3e9fc2973.png)\
+Usando como referencia la implementación de ItemDAO: MyBATISItemDao, cree el DAO MyBATIS de las demas entidades:\
+![image](https://user-images.githubusercontent.com/98216838/160671573-0fc12885-161e-4be9-af7a-cd17309826b0.png)\
+Cree la interface ServiciosAlquiler para los servicios de la capa lógica:\
+![image](https://user-images.githubusercontent.com/98216838/160672132-832cd925-8d1e-4d45-a0c6-435b2e7ada1c.png)\
+Realice en la implementación de la capa lógica (ServiciosAlquilerImpl), inyecte los DAO que sean necesarios. Tome de ejemplo el ItemDAO.\
+Realice la implementación de un servicio Stub (ServiciosAlquilerItemsStub) para que se pueda probar la lógica facilmente sin la capa de persistencia:\
+![image](https://user-images.githubusercontent.com/98216838/160673201-1c69c788-4fa5-4bbc-9164-0326de51137c.png)\
+Pruebe el programa ‘Main’ suministrado, y con este rectifique que a través de la capa lógica se pueda consultar un cliente.\
+
+Implemente los métodos que sean necesarios en las interfaces de las entidades (DAO) y en sus implementaciones haciendo uso del DAO inyectado. Haga un programa para comprobar que la consulta de un cliente se haga correctamente, a través de la capa lógica.\
+
+## PARTE II - PRUEBAS
+
+PARTE III - CAPA PRESENTACIÓN
+
+3. Cree el listener con el paquete y nombre indicados de forma que se asocie la configuración de Guice y MyBatis a la inicialización del contexto de la aplicación en el servidor tomcat7 embebido, con el siguiente contenido inicial, resolviendo el ‘TODO’ (asociando la interfaz del servicio a la implementación Stub)
+
+  ![image](https://user-images.githubusercontent.com/78982514/161620858-065c9019-47d6-41fd-bec9-1b3f68206147.png)
+  
+5. Implementar la aplicación Web que permita agregar nuevos clientes a la videotienda, y registrar alquileres para los mismos. Ambas funcionalidades estarán en dos vistas diferentes (registrocliente.xhtml, registroalquiler.xhtml), de acuerdo con las siguientes especificaciones (tenga en cuenta que, por ahora, la aplicación no maneja ningún esquema de autenticación)
+
+### Registro Alquiler
+ 
+![image](https://user-images.githubusercontent.com/78982514/161624436-9c08df6a-1b56-4473-b19e-a82024056050.png)
+![image](https://user-images.githubusercontent.com/78982514/161624472-2debe4e0-2b19-42a3-8bdc-d7a472b24935.png)
+ 
+
+### Registro Cliente
+
+![image](https://user-images.githubusercontent.com/78982514/161624330-af054d57-cf3a-4582-8ff4-aaf76d38e0c7.png)
+![image](https://user-images.githubusercontent.com/78982514/161624352-47831df2-e93c-448b-b3a8-df1d2b768546.png)
+
--- src/test/java/ServiciosAlquilerTest.java
+++ src/test/java/ServiciosAlquilerTest.java
@@ -1,73 +1,149 @@
-package edu.eci.cvds.test;
-
-import static org.junit.Assert.assertNull;
-
-
-import java.util.ArrayList;
+import java.text.SimpleDateFormat;
 import java.util.List;
-
 import com.google.inject.Inject;
-import edu.eci.cvds.sampleprj.dao.PersistenceException;
 import edu.eci.cvds.samples.entities.Cliente;
-import edu.eci.cvds.samples.entities.ItemRentado;
+import edu.eci.cvds.samples.entities.Item;
+import edu.eci.cvds.samples.entities.TipoItem;
 import edu.eci.cvds.samples.services.ExcepcionServiciosAlquiler;
 import edu.eci.cvds.samples.services.ServiciosAlquiler;
 import edu.eci.cvds.samples.services.ServiciosAlquilerFactory;
-import org.apache.ibatis.session.SqlSession;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.Assert;
 
 public class ServiciosAlquilerTest {
 
-    @Inject
-    private SqlSession sqlSession;
+    /*@Inject
+    private SqlSession sqlSession;*/
 
-    ServiciosAlquiler serviciosAlquiler;
+    @Inject
+    private ServiciosAlquiler serviciosAlquiler;
 
     public ServiciosAlquilerTest() {
         serviciosAlquiler = ServiciosAlquilerFactory.getInstance().getServiciosAlquilerTesting();
     }
 
-    @Before
-    public void setUp() {
+
+    @Test
+    public void deberiaRegistrarItem() {
+        TipoItem tipoItem = new TipoItem(3, "Peliculas");
+        SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            java.sql.Date sqlfechaLanzamiento = new java.sql.Date(formato.parse("2021-05-12").getTime());
+            Item newItem = new Item (tipoItem, 2162844, "Space Jam","Película netflix del 2021", sqlfechaLanzamiento, 2000, "DVD", "Infantil");
+            serviciosAlquiler.registrarItem(newItem);
+            Assert.assertEquals(serviciosAlquiler.consultarItem(2162844).getId(), 2162844);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
-    public void noDeberiaConsultarcliente() throws ExcepcionServiciosAlquiler {
-        assertNull(serviciosAlquiler.consultarCliente(1));
-    }
-    @Test
-    public void deberiaConsultarCliente(){
+    public void noDeberiaRegistrarItem() {
+        TipoItem tipoItem = new TipoItem(3, "Peliculas");
+        SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd");
         try {
-            serviciosAlquiler.consultarCliente(2233);
+            java.sql.Date sqlfechaLanzamiento = new java.sql.Date(formato.parse("2021-05-12").getTime());
+            java.sql.Date sqlfechaLanzamiento2 = new java.sql.Date(formato.parse("2019-01-17").getTime());
+            Item newItem = new Item (tipoItem, 2, "Space Jam","Película netflix del 2021", sqlfechaLanzamiento, 2000, "DVD", "Infantil");
+            Item newItem2 = new Item (tipoItem, 2, "Cómo entrenar a tu dragón 3","Película del 2019", sqlfechaLanzamiento2, 1500, "DVD", "Aventura");
+            serviciosAlquiler.registrarItem(newItem);
+            serviciosAlquiler.registrarItem(newItem2);
+        } catch (Exception e) {
+            Assert.assertEquals(e.getMessage(), "Se produjo un error al registrar el item.");
+        }
+    }
+
+    @Test
+    public void deberiaConsultarItem() {
+        TipoItem tipoItem = new TipoItem(3, "Peliculas");
+        SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            java.sql.Date sqlfechaLanzamiento = new java.sql.Date(formato.parse("2021-06-21").getTime());
+            Item newItem = new Item (tipoItem, 20, "A todo Gas","Película del 2021", sqlfechaLanzamiento, 2000, "DVD", "Acción");
+            serviciosAlquiler.registrarItem(newItem);
+            Assert.assertEquals(serviciosAlquiler.consultarItem(20).getNombre(), "A todo Gas");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void noDeberiaConsultarItem() {
+        try {
+            serviciosAlquiler.consultarItem(2);
+        } catch (Exception e) {
+            Assert.assertEquals(e.getMessage(), "Se produjo un error al consultar el item con id 22");
+        }
+    }
+
+    @Test
+    public void deberiaConsultarItems() {
+        TipoItem tipoItem = new TipoItem(3, "Peliculas");
+        SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            java.sql.Date sqlfechaLanzamiento = new java.sql.Date(formato.parse("2021-08-21").getTime());
+            java.sql.Date sqlfechaLanzamiento1 = new java.sql.Date(formato.parse("2019-01-17").getTime());
+            Item newItem = new Item (tipoItem, 1, "A todo Gas","Película del 2021", sqlfechaLanzamiento, 2000, "DVD", "Acción");
+            Item newItem2 = new Item (tipoItem, 2, "Cómo entrenar a tu dragón 3","Película del 2019", sqlfechaLanzamiento1, 1500, "DVD", "Aventura");
+            serviciosAlquiler.registrarItem(newItem);
+            serviciosAlquiler.registrarItem(newItem2);
+            List<Item> itemsDisponibles = serviciosAlquiler.consultarItemsDisponibles();
+            //System.out.println(itemsDisponibles);
+            if (itemsDisponibles != null) {
+                Assert.assertEquals(serviciosAlquiler.consultarItemsDisponibles().get(0).getNombre(), "A todo Gas");
+                Assert.assertEquals(serviciosAlquiler.consultarItemsDisponibles().get(1).getNombre(), "Cómo entrenar a tu dragón 3");
+            } else {
+                Assert.assertTrue(false);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void deberiaActualizarTarifaItem() {
+        TipoItem tipoItem = new TipoItem(3, "Peliculas");
+        SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            java.sql.Date sqlfechaLanzamiento = new java.sql.Date(formato.parse("2021-05-12").getTime());
+            Item newItem = new Item (tipoItem, 2162845, "Space Jam","Película netflix del 2021", sqlfechaLanzamiento, 2000, "DVD", "Infantil");
+            serviciosAlquiler.registrarItem(newItem);
+            serviciosAlquiler.actualizarTarifaItem(2162845, 3000);
+            Assert.assertEquals(serviciosAlquiler.consultarItem(2162845).getTarifaxDia(), 3000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void deberiaRegistrarCliente() {
+        try {
+            Cliente cliente = new Cliente("Fernando", 16254827, "3232452466", "Cra. 2 - 45", "fernando@mail.com", false, null);
+            serviciosAlquiler.registrarCliente(cliente);
+            //System.out.println(serviciosAlquiler.consultarCliente(16254827));
+            Assert.assertEquals(serviciosAlquiler.consultarCliente(16254827).getDocumento(), 16254827);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void deberiaConsultarCliente() {
+        try {
+            Assert.assertEquals(serviciosAlquiler.consultarCliente(16254827).getDocumento(), 16254827);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void noDeberiaConsultarElCostoDeUnItemDesconocido() throws ExcepcionServiciosAlquiler{
+        try {
+            Assert.assertEquals(101010101, serviciosAlquiler.consultarCostoAlquiler(91919191, 818181));
+            Assert.fail("No entro a la excepcion");
+        } catch (Exception e) {
             Assert.assertTrue(true);
-        } catch (ExcepcionServiciosAlquiler ex) {
-            Assert.fail();
         }
     }
-    @Test
-    public void deberiaRegistrarcliente(){
-        try {
-            ArrayList<ItemRentado> itemRentados = new ArrayList<ItemRentado>();
-            Cliente cliente = new Cliente("david",2168514,"telefono","direccion","email",false,itemRentados);
-            serviciosAlquiler.registrarCliente(cliente);
-            Assert.assertTrue("david".equals(serviciosAlquiler.consultarCliente(2168514).getNombre()));
-        } catch (ExcepcionServiciosAlquiler ex) {
-            Assert.fail();
-        }
-    }
-    @Test
-    public void NodeberiaRegistrarclienterepetido(){
-        try {
-            ArrayList<ItemRentado> itemRentados = new ArrayList<ItemRentado>();
-            Cliente cliente = new Cliente("david",2168514,"telefono","direccion","email",false,itemRentados);
-            serviciosAlquiler.registrarCliente(cliente);
-        } catch (ExcepcionServiciosAlquiler ex) {
-            Assert.fail();
-        }
-    }
-    
-    
-    
 }
